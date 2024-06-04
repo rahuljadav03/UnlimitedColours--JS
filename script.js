a1a2a574c7f454eddbff118ffaf95c34b7508422
Rahul Jadav <114642613+rahuljadav03@users.noreply.github.com>
2024-06-04 14:54:01 +0530

    Added Files

---
+++ script.js
@@ -0,0 +1,23 @@
+const randomColor = function()
+{
+    const hex = "0123456789ABCDEF";
+    let color = "#";
+    for (let i = 0; i < 6; i++) {
+        color += hex[Math.floor(Math.random()*16)];        
+    }
+    return color;
+}
+
+let intervalId;
+document.querySelector('#start').addEventListener('click',function(){
+   if (!intervalId) {
+    intervalId = setInterval(function(){
+        document.body.style.backgroundColor = randomColor();
+    },1000);
+   }
+});
+
+document.querySelector('#stop').addEventListener('click',function(){
+    clearInterval(intervalId);
+    intervalId = null;
+});
